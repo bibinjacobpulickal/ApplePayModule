c97c15f7e950e7db0572ebdf1152a2548cecd12d
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-07-16 15:53:47 +0530

    Initial Commit

---
+++ ApplePayModule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApplePayModule
+//
+//  Created by Bibin Jacob Pulickal on 16/07/20.
+//  Copyright © 2020 bibinjacobpulickal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
